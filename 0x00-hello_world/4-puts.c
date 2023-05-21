33b6f596afe8a40c3842a0454e29c5c3bc6b866d
sam <sami@gmail.com>
2023-05-21 00:35:16 -0700

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-int main()
+
+int main(void)
+
 {
-	
-    char Mystr[] = "Programming is like building a multilingual puzzle,";
-    
-    puts(Mystr);
-    
-    return 0;
+	puts ("\"Programming is like building a multilingual puzzle\n");
+	return 0;
 }
