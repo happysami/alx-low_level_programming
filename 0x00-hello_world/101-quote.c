56ee1401c4c72558d3bdca43dc57280c63bd7f53
sam <sami@gmail.com>
2023-05-22 14:36:46 -0700

    all

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
+/**
+ * main - main function
+ * Return: is 0
+ */
 int main(void)
 {
-	char *txt = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
+char *txt = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, txt, 59);
 	return (1);
 }
-		
-
