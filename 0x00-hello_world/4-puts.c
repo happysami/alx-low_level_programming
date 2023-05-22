8c9a72894353c39461135368e5015a5d91567c6f
sam <sami@gmail.com>
2023-05-22 07:39:05 -0700

    my first c program eith puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - main func
- * Return: Allways 0
-=======
- * main - A program that print aline puts function
- * main: A program that print a line
- * return : 0 (success)
- * voide:Is empity value
->>>>>>> 07a8552cb859c95cacfc2f9bba515b3aed49c0cd
+ * main - main function
+ * Return: return 0
  */
 int main(void)
 {
