6484f958a8e69c5f1919dc1fb87f3fd11007c1f0
sam <sami@gmail.com>
2023-05-22 07:13:51 -0700

    my first c programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void);
+/**
+ * main - main func
+ * Return: Allways 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
