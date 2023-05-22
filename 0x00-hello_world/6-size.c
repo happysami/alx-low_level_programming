0651ca73ec9cd976be55a5314ea9e57e9208d429
sam <sami@gmail.com>
2023-05-22 11:36:14 -0700

    size of various

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ char c;
 float f;
 long int l;
 long long int a;
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-printf("Size of a int: %lu.\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu.\n", (unsigned long)sizeof(f));
-printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(l));
-printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu Byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a int: %lu  Byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu  Byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long long int: %lu  Byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a float: %lu  Byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
