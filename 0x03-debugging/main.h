40aafe32364f88d115895e4302de260145978ba5
sam <sami@gmail.com>
2023-05-24 13:49:34 -0700

    1. Like, comment, subscribeCopy this main file. Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int kargest_number(int a, int b, int day);
+void print_reasining_day(int month, int day, int year);
+
+#endif /* MAIN_H */
