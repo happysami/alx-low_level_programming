c58b50edee184f8e1183a1cd879ace9708a5d9f7
sam <sami@gmail.com>
2023-05-26 12:38:01 -0700

    all

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * mul - Multipul two int
  * @x:The first int
  * @y:The second int
  * Return:The return value will be x or y
  */
-int int mul(int x, int y);
+int mul(int x, int y)
 {
 	return (x * y);
 }
