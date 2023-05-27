ec41395fa71d6213376d636b1b7945b71a039530
sam <sami@gmail.com>
2023-05-27 12:17:21 -0700

    this is my c programing that print z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this program is desplay a-y
+ * Return: the return value is 0
+ */
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{prinf("%c", i);
+}
+printf " \n ");
+return (0);
+}
